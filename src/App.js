1d527a50fa5f6a13684373ca65ba3d628271c6c5
Mehdi Khediri <mehdibigf@gmail.com>
2021-05-04 12:06:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './Images/imageInSrc.png'
+import { Button , Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+ <div>
+  <div>
+    <h1 className= "title" > Mehdi Khediri </h1>
+    <br />
+    <div className = "imgs">
+    <img src={image} className = "img1" />
+    <br />
+    <br />
+    <br />
+    <img src={'/image/imageInPublic.png'} className = "img2" />
+    </div>
+    <br />
+    <br />
+    <br />
+    <video width={320} height={240} controls className = "img2" >
+    <source src={'/GMC.mp4'} />
+  </video>
+  </div>
+  <br />
+  <br />
+  <br />
+  
+  </div>
+
+
     </div>
   );
 }
